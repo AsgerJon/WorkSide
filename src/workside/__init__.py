848f86072257baf4fc68fbbb0a3fbb3945c00988
AsgerJon <asgerjon2@gmail.com>
2023-06-11 23:19:43 +0200

    initial commit

---
+++ src/workside/__init__.py
@@ -0,0 +1,9 @@
+"""WorkSide streamlines your use of PySide6 enabling your access to Qt from
+Python!"""
+#  MIT Licence
+#  Copyright (c) 2023 Asger Jon Vistisen
+from __future__ import annotations
+
+from icecream import ic
+
+ic.configureOutput(includeContext=True)
